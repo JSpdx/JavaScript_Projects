ff1b27501128ebe02730956597224e2eab52b91b
JS_PDX <57430576+DrFunkenstein@users.noreply.github.com>
2020-01-22 16:40:35 -0800

    HTML_2 assignments

---
+++ JS/Basic_JavaScript_2.js
@@ -0,0 +1,17 @@
+
+//changing color with button assignmnet//
+function turnRed() {
+  var str = "Because they're red!! (I'm so sorry)"
+  var strRed = str.fontcolor("red");
+  document.getElementById("button").innerHTML = strRed;
+} 
+
+//Concatenating variable assignment//
+
+function coolFunctionBro() {
+    var sentence = "This sentence"
+    sentence += " was made by"
+    sentence += " concatenating strings"
+    document.getElementById("concatenate").innerHTML = sentence;
+}
+
